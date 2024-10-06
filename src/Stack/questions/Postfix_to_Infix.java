619923d167eff62bb8c8574dad9a21fbc4b3b959
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2024-10-06 11:54:05 +0530

    postfix to infix conversion

---
+++ src/Stack/questions/Postfix_to_Infix.java
@@ -0,0 +1,42 @@
+package Stack.questions;
+
+import java.util.Stack;
+
+// import Stack.Implementation.UsingArrays.Stack;
+
+public class Postfix_to_Infix {
+
+    public static boolean isOperand(char c) {
+
+        if(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) return true;
+
+        else return false;
+
+    }
+    public static String convertToInfix(String exp) {
+
+        Stack<String> s = new Stack<String>(); 
+
+        for(int i = 0; i < exp.length(); i++) {
+            if(isOperand(exp.charAt(i))) {
+                s.push(String.valueOf(exp.charAt(i)));
+            }
+
+            else {
+                String op1 = s.peek();
+                s.pop();
+                String op2 = s.peek();
+                s.pop();
+                s.push("(" + op2 + exp.charAt(i) + op1 + ")");
+            }
+        }
+        
+        return s.peek();
+    }
+    public static void main(String[] args) {
+        
+        String exp = "ab*c+";
+        System.out.println(convertToInfix(exp));
+    }
+    
+}
