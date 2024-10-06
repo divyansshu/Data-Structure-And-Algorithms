f6f4f3cf3c8e2abae838e6d44359cb5459df09e2
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2024-10-07 00:33:05 +0530

    fibnocci series and optimizing the run time by storing the values in an array

--- src/Algorthims/recursion/Fibonacci.java
+++ src/Algorthims/recursion/Fibonacci.java
@@ -1,23 +1,34 @@
 package Algorthims.recursion;
 
+import java.util.Arrays;
+
 public class Fibonacci {
 
-    public static int fib(int n) {
+    public static int fib(int[] sol, int n) {
 
         if(n == 0)
             return 0;
 
         if(n == 1)
             return 1;
+        if(n == 2)
+            return 1;
 
-        return fib(n-2) + fib(n-1);
+        if(sol[n] != -1) return sol[n];
+
+        sol[n] = fib(sol, n-2) + fib(sol, n-1);
+
+        return sol[n];
+
     }
     public static void main(String[] args) {
 
-        int n = 8;
+        int n = 45;
+        int[] sol = new int[n+1];
+        Arrays.fill(sol, -1);
 
-        for(int i = 0; i <= n; i++) {
-            System.out.print(fib(n));
-        }
+//        for(int i = 0; i <= n; i++) {
+            System.out.print(fib(sol, n));
+//        }
     }
 }
