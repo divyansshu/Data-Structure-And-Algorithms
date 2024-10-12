4d2b9f86be88c52384f4b9963a1a2bfa3d6cb278
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2024-10-12 23:25:53 +0530

    implementation of queue using stacks

--- src/queue/questions/QueueUsingTwoStacks/Queuey.java
+++ src/queue/questions/QueueUsingTwoStacks/Queuey.java
@@ -1,45 +1,55 @@
 package queue.questions.QueueUsingTwoStacks;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
 public class Queuey {
 
-    static Stack<Integer> s1 = new Stack<>();
+   static Stack<Integer> s1 = new Stack<>();
    static Stack<Integer> s2 = new Stack<>();
 
+   //enqueue operation time Complexity: O(1)
     public static void enqueue(int x) {
 
         s1.push(x);
     }
 
+    //dequeue time complexity: O(n)
     public static int dequeue() {
+       while(!s1.isEmpty()) {
+        s2.push(s1.pop());
+       }
+       int popped = s2.pop();
 
-        if(s1.isEmpty() && s2.isEmpty()) {
-            return -1;
+       while(!s2.isEmpty()) {
+        s1.push(s2.pop());
+       }
+       return popped;
+    }
+
+    public static void display() {
+
+        while(!s1.isEmpty()) {
+            s2.push(s1.pop());
         }
 
-        if(s2.isEmpty()) {
-            while(!s1.isEmpty()) {
-                s2.push(s1.peek());
-                s1.pop();
-            }
+        while(!s2.isEmpty()) {
+            System.out.println(s2.peek());
+            s1.push(s2.pop());
         }
 
-        int xx = s2.pop();
-        return xx;
     }
 
     public static void main(String[] args) {
 
-        Queuey q = new Queuey();
-        q.enqueue(10);
-        q.enqueue(20);
-        q.enqueue(30);
-        q.enqueue(40);
-        q.enqueue(50);
-        System.out.println(q.dequeue());
+
+        Queuey.enqueue(10);
+        Queuey.enqueue(20);
+        Queuey.enqueue(30);
+        Queuey.enqueue(40);
+        Queuey.enqueue(50);
+        Queuey.display();
+        System.out.println("popped element from Queue is: " +Queuey.dequeue());
+        Queuey.display();
 
     }
 }
