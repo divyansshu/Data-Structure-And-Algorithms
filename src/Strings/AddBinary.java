ad58611c7ef691f77ad7a980085613ad140b84ff
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2024-11-07 23:55:38 +0530

    add binary numbers better approach

--- src/Strings/AddBinary.java
+++ src/Strings/AddBinary.java
@@ -19,11 +19,36 @@ public class AddBinary {
 
         return Integer.toBinaryString(a);
     }
+
+    /*
+     * This implementation handles binary addition directly on the strings and
+     * avoids potential issues with integer overflow.
+     * time complextiy: O(n)
+     * space complexity: O(n)
+     */
+    public static String addBinary_2(String x, String y) {
+        StringBuffer result = new StringBuffer();
+        int i = x.length() - 1;
+        int j = y.length() - 1;
+        int carry = 0;
+
+        while(i >= 0 || j >= 0 || carry != 0) {
+            int bit1 = (i >= 0) ? x.charAt(i) - '0' : 0;
+            int bit2 = (j >= 0) ? y.charAt(i) - '0' : 0;
+
+            int sum = bit1 + bit2 + carry;
+            result.append(sum % 2);
+            carry = sum /2;
+        }
+
+        return result.reverse().toString();
+    }
     public static void main(String[] args) {
         String a = "1010";
         String b = "1011";    
 
         System.out.println(addBinary(a,b));
+        System.out.println(addBinary_2(a, b));
     }
     
 }
