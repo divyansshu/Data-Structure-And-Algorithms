ad613d7c00fe499e7e50e6649159568254b88498
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2024-11-20 01:11:01 +0530

    created graph using adjacency list

---
+++ src/graph/implementation/adjacencyList/Graph.java
@@ -0,0 +1,50 @@
+package graph.implementation.adjacencyList;
+
+import java.util.ArrayList;
+
+public class Graph {
+
+    static class Edge {
+        int src;
+        int dest;
+        int weight;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.weight = w;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        //declare and initialize vertex of graph
+        int v = 5;
+
+        ArrayList<Edge>[] graph = new ArrayList[v];
+
+        for(int i = 0; i < v; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        //0-vertex
+        graph[0].add(new Edge(0,1 , 5));
+
+        //1-vertex
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        // 2-vertex
+        graph[2].add(new Edge(2, 4, 2));
+        graph[2].add(new Edge(2, 3, 1));
+
+        // 3-vertex
+        graph[3].add(new Edge(3, 1, 3));
+        graph[3].add(new Edge(3, 2, 1));
+
+        // 4-vertex
+        graph[4].add(new Edge(4, 2, 2));
+    }
+    
+}
