f18c20e99b150b471e87875a5874a2b8fc87fb3b
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2024-12-23 21:31:46 +0530

    valid perfect square

---
+++ src/Math/ValidPerfectSquare.java
@@ -0,0 +1,32 @@
+public class ValidPerfectSquare {
+
+    // Method to check if a number is a perfect square
+    public static boolean perfectSquare(int n) {
+        int left = 1;
+        int right = n;
+
+        // Binary search to find the perfect square
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            
+            //The result is cast to a long to handle large numbers and avoid overflow.
+            long midSquare = (long) mid * mid;
+
+            // Check if midSquare is equal to n
+            if(midSquare == n) return true;
+            // If midSquare is less than n, search in the right half
+            else if(midSquare < n) left = mid + 1;
+            // If midSquare is greater than n, search in the left half
+            else right = mid - 1;
+        }
+        // Return false if no perfect square is found
+        return false;
+    }
+
+    // Main method to test the perfectSquare method
+    public static void main(String[] args) {
+        int n = 31;
+        System.out.println(perfectSquare(n)); // Output: false
+    }
+    
+}
