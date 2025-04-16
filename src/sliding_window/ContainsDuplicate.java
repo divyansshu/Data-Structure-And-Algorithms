b6edb2f54b4a20d27e775560783d2c2f368c1693
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2025-04-16 23:42:02 +0530

    moved out sliding window folder

---
+++ src/sliding_window/ContainsDuplicate.java
@@ -0,0 +1,32 @@
+package sliding_window;
+
+class ContainsDuplicate {
+
+    public static boolean containsNearbyDuplicate(int[] arr, int k) {
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+
+            if (j > i) {
+                if (arr[i] == arr[j]) {
+                    if (Math.abs(i - j) <= k)
+                        return true;
+                    else
+                        j--;
+                } else {
+                    j--;
+                }
+            } 
+            else {
+                j = arr.length - 1;
+                i++;
+            }
+        }      
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,0,1,1};
+        System.out.println(containsNearbyDuplicate(arr,1));
+    }
+}
