b6985ce096833642340aba7e0ee85b79a79cdbba
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2025-06-03 16:36:51 +0530

    moved to binary search folder

---
+++ src/Algorthims/Searching/Binary_search/Minimum_in_sorted_rotated_array.java
@@ -0,0 +1,35 @@
+package Algorthims.Searching.Binary_search;
+
+public class Minimum_in_sorted_rotated_array {
+
+    public static int min_in_rotated_sorted_array(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        
+        while(low < high) {
+
+            if(arr[low] < arr[high]) {
+                return low;
+            }
+
+           int mid = (low+high)/2;
+
+            if(arr[mid] > arr[high]) {
+                low = mid + 1;
+            }
+            else 
+                high = mid;
+            
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+
+        // rotated sorted array
+        int[] arr= {4, 5, 6, 9, 0, 1, 2};
+        int[] arr2 = {5,6,1,2,3,4};
+ 
+        int min = min_in_rotated_sorted_array(arr2);
+        System.out.println("minimum element in rotated sorted array is: "+ arr2[min]+ " at index "+ min);
+    }
+}
