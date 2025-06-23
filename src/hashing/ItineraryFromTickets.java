d693c70c3c2b150469545a587c792d022650c8e6
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2025-06-23 09:25:39 +0530

    Adds hashing-based utility methods for arrays and maps
    Introduces four new classes implementing hashing-based solutions:
    
    1. Calculates the intersection of two arrays.
    2. Derives itineraries from ticket data using a hashmap.
    3. Identifies elements in an array with occurrences above a threshold.
    4. Computes the union of two arrays.
    
    These additions enhance functionality for common data manipulation tasks.

---
+++ src/hashing/ItineraryFromTickets.java
@@ -0,0 +1,49 @@
+package hashing;
+
+import java.util.HashMap;
+
+public class ItineraryFromTickets {
+
+    public static void findItinerary(HashMap<String,String> tickets) {
+
+        //reversing the hashmap key = value, value = key
+        HashMap<String, String> revTickets = new HashMap<>();
+        for(String key: tickets.keySet()) {
+
+            revTickets.put(tickets.get(key), key);
+        }
+
+        System.out.println(revTickets);
+        System.out.println();
+
+        //finding starting location
+        String start="";
+        for (String key: tickets.keySet()) {
+
+            if(revTickets.containsKey(key) == false) {
+                start = key;
+            }
+        }
+
+        //printing the path form starting to end
+        System.out.print(start);
+        while(tickets.containsKey(start)) {
+            System.out.print("->"+tickets.get(start));
+            start = tickets.get(start);
+        }
+
+    }
+    public static void main(String[] args) {
+
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+        System.out.println(tickets);
+        System.out.println();
+
+        findItinerary(tickets);
+    }
+}
